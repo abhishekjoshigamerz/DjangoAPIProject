b6737c9327e280343eef77e97d9731a7a8db5077
abhishekjoshigamerz <69464702+abhishekjoshigamerz@users.noreply.github.com>
2020-11-10 14:44:01 +0530

    Commited! Registration remaning project

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'posts', views.BlogPostViewSet)
+router.register(r'users', views.UserViewSet)
+#router.register(r'registeruser',views.RegisterViewSet)
+
+urlpatterns = [
+    path(r'api/', include(router.urls)),
+    path(r'', views.index, name='index')
+]
